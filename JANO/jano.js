f274c78c5a8170a797a482658dc99145da8667d0
Noe <noe.muanza@gmail.com>
2024-06-05 17:12:50 +0200

    jano

---
+++ JANO/jano.js
@@ -0,0 +1,8 @@
+const nono = document.querySelector('.nono');
+
+nono.addEventListener('click', function() {
+    console.log('nono');
+    //redirect towars puzzle.html
+    window.location.href = 'puzzle.html';
+});
+
